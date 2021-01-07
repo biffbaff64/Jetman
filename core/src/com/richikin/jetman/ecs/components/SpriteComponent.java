48938b0d900e30cf7b17e23d89558cdc3ee1b802
biffbaff64 <richikin@gmail.com>
2021-01-07 22:53:58 +0000

    Ashley ECS Experiment

--- core/src/com/richikin/jetman/ecs/components/SpriteComponent.java
+++ core/src/com/richikin/jetman/ecs/components/SpriteComponent.java
@@ -7,11 +7,19 @@ public class SpriteComponent implements Component
 {
     public Sprite sprite;
 
+    public int spriteNumber;
+    public int strength;
+
     public float   rotation    = 0.0f;
+    public boolean isRotating  = false;
+    public float   rotateSpeed = 0.0f;
+
     public boolean isFlippedX  = false;
     public boolean isFlippedY  = false;
     public boolean isDrawable  = false;
     public boolean isAnimating = false;
+    public boolean isLinked    = false;
+    public boolean isEnemy     = false;
 
     public int frameWidth;
     public int frameHeight;
--- core/src/com/richikin/jetman/ecs/components/TransformComponent.java
+++ core/src/com/richikin/jetman/ecs/components/TransformComponent.java
@@ -3,9 +3,15 @@ package com.richikin.jetman.ecs.components;
 import com.badlogic.ashley.core.Component;
 import com.richikin.utilslib.maths.SimpleVec2F;
 import com.richikin.utilslib.maths.SimpleVec3;
+import com.richikin.utilslib.maths.XYSetF;
+import com.richikin.utilslib.physics.Direction;
 
 public class TransformComponent implements Component
 {
-    public final SimpleVec3  position = new SimpleVec3();
-    public final SimpleVec2F scale    = new SimpleVec2F(1.0f, 1.0f);
+    public final SimpleVec3  position  = new SimpleVec3();
+    public final SimpleVec2F scale     = new SimpleVec2F(1.0f, 1.0f);
+    public final Direction   direction = new Direction();
+    public final Direction   lookingAt = new Direction();
+    public final SimpleVec2F speed     = new SimpleVec2F();
+    public final XYSetF      distance  = new XYSetF();
 }
